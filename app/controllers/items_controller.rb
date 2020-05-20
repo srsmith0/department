64131e4342fdc5ab6f84d2a9c5e516aafbe52fcf
Shawn Smith <srsmith11@live.com>
2020-05-20 16:16:55 -0600

    updated item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,15 @@
 class ItemsController < ApplicationController
   before_action :set_department
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  
   def index
     @department = Department.find(params[:department_id])
     @items = @department.items
   end
-
   def show
   end
-
   def new
     @item = @department.items.new(item_params)
   end
-
   def create
     @item = @department.items.new(item_params)
     if @item.save
@@ -22,10 +18,8 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-
   def edit
   end
-
   def update
     if @item.update(item_params)
       redirect_to [@department, @item]
@@ -33,23 +27,18 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
-
   def destroy
     @item.destroy
     redirect_to department_items_path(@department)
-  end 
-
+  end
   private
   def item_params
     params.require(:item).permit(:name, :price, :description)
   end
-
   def set_department
     @department = Department.find(params[:id])
   end
-
   def set_item
     @item = Item.find(params[:id])
   end
-
 end
